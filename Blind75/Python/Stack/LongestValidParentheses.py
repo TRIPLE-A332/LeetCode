0d1415002ad5059a2248c62dc1f3e834532c669c
TRIPLE-A332 <abdullah.ahmad16j@gmail.com>
2025-07-27 22:42:48 -0400

    binarySearch

--- Blind75/Python/Stack/LongestValidParentheses.py
+++ Blind75/Python/Stack/LongestValidParentheses.py
@@ -1,9 +1,21 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        stack=[]
+        stack=[-1]
         maxLen = 0
+        length = 0
 
         for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    length = i - stack[-1]
+                    maxLen = max(length, maxLen)
+        
+        return maxLen
             
 
 if __name__ == "__main__":
