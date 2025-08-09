c4d88daa40c764164c9b6892622f94e49c2dbf55
TRIPLE-A332 <abdullah.ahmad16j@gmail.com>
2025-08-09 18:09:15 -0400

    Trees

---
+++ Blind75/Python/Trees/MaxDepthBinaryTree.py
@@ -0,0 +1,7 @@
+class Solution:
+    count = 0 
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
